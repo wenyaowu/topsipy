72b9f93d7207a9fbe767663148217dfbf2611940
wenyaowu <a129441442@gmail.com>
2015-04-15 15:58:28 -0700

    Update all the Topsy content API features. ( tweets() still need to be fixed)

--- topsipy/client.py
+++ topsipy/client.py
@@ -95,13 +95,13 @@ class Topsipy(object):
         else:
             return None
 
-    def _get(self, url, args=None, payload=None, **kwargs):
+    def _get(self, get_url, args=None, payload=None, **kwargs):
         if args:
             kwargs.update(args)
         retries = self.max_get_retries
         while retries > 0:
             try:
-                return self._internal_call('GET', url, payload, kwargs)
+                return self._internal_call('GET', get_url, payload, kwargs)
             except TopsipyExcepetion as e:
                 if 500 <= e.http_status < 600:  # Server error
                     retries -= 1
@@ -166,7 +166,20 @@ class Topsipy(object):
         """
         return self._get('content/tweet.json', postids=','.join(postids))
 
-    def top_tweets(self, q=None, sort_by=None, offset=0, limit=5):
+    def bulktweets(self, q=None, limit=10, *args):
+        """
+        Provides a large set of tweets that match the specified query and filter parameters.
+        The maximum number of items returned per query is 20,000.
+        :param q:(optional)
+        Terms to retrieve trending posts on.
+        If no term is entered, then trending posts results are returned for all of Twitter.
+        :param limit:(optional)
+        Maximum number of results returned. (default: 10, max: 20000).
+        :return: tweets
+        """
+        return self._get('content/bulktweets.json', q=q, limit=limit, args=args)
+
+    def top_tweets(self, q=None, sort_by=None, offset=0, limit=5, *args):
         """
         Provides the top tweets for a list of terms and additional search parameters.
         The ranking is determined by the sort method.
@@ -181,5 +194,89 @@ class Topsipy(object):
         Maximum number of results returned. (default: 5, max: 500).
         :return: List of tweets
         """
-        return self._get('content/tweets.json', q=q, sort_by=sort_by, offset=offset, limit=limit)
+        return self._get('content/tweets.json', q=q, sort_by=sort_by, offset=offset, limit=limit, args=args)
 
+    def photos(self, q=None, sort_by=None, offset=0, limit=5, *args):
+        """
+        Provides the top photos for a list of terms and additional search parameters.
+        The ranking is determined by the sort method.
+        :param q: (optional)
+        Query term(s) to return matching results for.
+        If omitted, trending tweets from all Twitter are shown.
+        :param sort_by: (optional)
+        Sort Method
+        :param offset: (optional)
+        Offset from which to start the results. (default=0)
+        :param limit: (optional)
+        Maximum number of results returned. (default: 5, max: 500).
+        :return: List of photos
+        """
+        return self._get('content/photos.json', q=q, sort_by=sort_by, offset=offset, limit=limit, args=args)
+
+    def links(self, q=None, sort_by=None, offset=0, limit=5, *args):
+        """
+        Provides the top links for a list of terms and additional search parameters.
+        The ranking is determined by the sort method.
+        :param q: (optional)
+        Query term(s) to return matching results for.
+        If omitted, trending tweets from all Twitter are shown.
+        :param sort_by: (optional)
+        Sort Method
+        :param offset: (optional)
+        Offset from which to start the results. (default=0)
+        :param limit: (optional)
+        Maximum number of results returned. (default: 5, max: 500).
+        :return: List of links
+        """
+        return self._get('content/links.json', q=q, sort_by=sort_by, offset=offset, limit=limit, args=args)
+
+    def videos(self, q=None, sort_by=None, offset=0, limit=5, *args):
+        """
+        Provides the top videos for a list of terms and additional search parameters.
+        The ranking is determined by the sort method.
+        :param q: (optional)
+        Query term(s) to return matching results for.
+        If omitted, trending tweets from all Twitter are shown.
+        :param sort_by: (optional)
+        Sort Method
+        :param offset: (optional)
+        Offset from which to start the results. (default=0)
+        :param limit: (optional)
+        Maximum number of results returned. (default: 5, max: 500).
+        :return: List of videos
+        """
+        return self._get('content/videos.json', q=q, sort_by=sort_by, offset=offset, limit=limit, args=args)
+
+    def citations(self, url=None, sort_by=None, offset=0, limit=5, *args):
+        """
+        Returns the full trackback history of a URL in reverse chronological order (up to 10,000 results).
+        :param q_url: (optional)
+        URL for the target.
+        This may be a permalink for a tweet, or any other URL for a photo, video or link.
+        If omitted, trending tweets from all Twitter are shown.
+        :param sort_by: (optional)
+        Sort Method
+        :param offset: (optional)
+        Offset from which to start the results. (default=0)
+        :param limit: (optional)
+        Maximum number of results returned. (default: 5, max: 500).
+        :return: track back of a URL
+        """
+        return self._get('content/citations.json', url=url, sort_by=sort_by, offset=offset, limit=limit, args=args)
+
+    def conversation(self, url=None):
+        """
+        Provides the full reply thread to a specific tweet.
+        :param url:The permalink of the tweet.
+        :return: Reply thread to a tweet/URL
+        """
+        return self._get('content/conversation.json', url=url)
+
+    def validate(self, postid):
+        """
+        Returns true if a tweet is still valid, false otherwise.
+        This is useful to check for tweet deletions that occur after a tweet was first returned.
+        :param postid: A comma-separated list of tweet IDs.
+        :return: True or False
+        """
+        return self._get('content/validate.json', postid=postid)
