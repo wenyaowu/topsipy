a21a3843dd0d1d3d9e1e57cab84590fa63ca4688
wenyaowu <a129441442@gmail.com>
2015-04-15 22:54:03 -0700

    update setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'evanwu'
+from setuptools import setup
+
+setup(name='topsipy',
+  version='1.0',
+  description='A simple wrapper library for the Topsy APIs',
+  author='Evan Wu',
+  author_email='evanwu0225@gmail.com',
+  url='https://github.com/wenyaowu/topsipy',
+  packages=['topsipy'],
+  install_requires=['requests>=1.0']
+
+ )
